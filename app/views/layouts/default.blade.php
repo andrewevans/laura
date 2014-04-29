f66cbfc82f8edfa34729319b10539987adb00dd7
andrewe <andrewevansc@gmail.com>
2014-04-28 19:19:05 -0700

    default layout beefed up use bootstrap

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,12 +1,32 @@
-<!doctype html>
-<html lang="en">
-    <meta charset=""utf-8">
+<!DOCTYPE html>
+<html>
 <head>
-
+    <title>Look! I'm CRUDding</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 </head>
 <body>
+<div class="container">
+
+    <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ URL::to('nerds') }}">Default Stuff!</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="{{ URL::to('users') }}">View All Users</a></li>
+            <li><a href="{{ URL::to('users/create') }}">Create a User</a>
+        </ul>
+    </nav>
+
+    <h1>All the Users</h1>
+
+    <!-- will be used to show any messages -->
+    @if (Session::has('message'))
+    <div class="alert alert-info">{{ Session::get('message') }}</div>
+    @endif
 
     @yield('content')
 
+
+</div>
 </body>
 </html>
