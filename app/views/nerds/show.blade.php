700d7fdf9af14d649585eec531ec25096f5d106d
andrewe <andrewevansc@gmail.com>
2014-04-28 19:18:50 -0700

    nerds using layout

--- app/views/nerds/show.blade.php
+++ app/views/nerds/show.blade.php
@@ -1,23 +1,7 @@
+@extends('layouts.nerds')
 <!-- app/views/nerds/show.blade.php -->
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Look! I'm CRUDding</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-
-    <nav class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="{{ URL::to('nerds') }}">Nerd Alert</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('nerds') }}">View All Nerds</a></li>
-            <li><a href="{{ URL::to('nerds/create') }}">Create a Nerd</a>
-        </ul>
-    </nav>
+@section('content')
 
     <h1>Showing {{ $nerd->name }}</h1>
 
@@ -29,6 +13,4 @@
         </p>
     </div>
 
-</div>
-</body>
-</html>
+@stop
